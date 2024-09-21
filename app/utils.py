c234e6e6fe4b6128dba5e1690009d3ecef134bb1
Esraafathysayed <esraafathy79295@gmail.com>
2024-09-22 00:58:35 +0300

    CRT: create utils file with func needed

---
+++ app/utils.py
@@ -0,0 +1,19 @@
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from .models import User
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = db.query(User).filter(User.email == email).first()
+    if not user or not verify_password(password, user.password):
+        return False
+    return user
