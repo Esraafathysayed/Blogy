9a95f943690f0e42723dd8122f05f3abaac55ed9
Esraafathysayed <esraafathy79295@gmail.com>
2024-09-24 22:20:15 +0300

    DOC: add docs to post routes

--- app/api/v1/routers/post.py
+++ app/api/v1/routers/post.py
@@ -15,6 +15,14 @@ router = APIRouter(prefix="/api/v1/posts", tags=["Posts"])
 
 @router.get("/", response_model=List[schemas.PostWDetailsResponse])
 def get_posts(page_num: int =  1, page_size: int = 10, db: Session = Depends(get_db)):
+    """Get all posts
+    Args:
+        page_num (int, optional): Page number. Defaults to 1.
+        page_size (int, optional): Page size. Defaults to 10.
+        db (Session, optional): Database session. Defaults to Depends(get_db).
+    Returns:
+        List[schemas.PostWDetailsResponse]: List of posts
+    """
     start = (page_num - 1) * page_size
     end = start + page_size
 
@@ -30,6 +38,16 @@ def get_posts(page_num: int =  1, page_size: int = 10, db: Session = Depends(get
 
 @router.get("/{id}", response_model=schemas.PostWDetailsResponse)
 def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
+    """Get a post
+    Args:
+        id (int): Post ID
+        db (Session, optional): Database session. Defaults to Depends(get_db).
+        current_user (int, optional): Current user. Defaults to Depends(get_current_user).
+    Raises:
+        HTTPException: [description]
+    Returns:
+        schemas.PostWDetailsResponse: Post details
+    """
     likes_subquery = select(func.count(Likes.id).label("likes"), Likes.post_id)\
         .group_by(Likes.post_id).subquery()
 
@@ -48,6 +66,14 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostCreateUpdateResponse)
 def create_post(post: schemas.PostCreateUpdateRequest, db: Session = Depends(get_db),
                 current_user: int = Depends(get_current_user)):
+    """Create a post
+    Args:
+        post (schemas.PostCreateUpdateRequest): Post data
+        db (Session, optional): Database session. Defaults to Depends(get_db).
+        current_user (int, optional): Current user. Defaults to Depends(get_current_user).
+    Returns:
+        Post: Post object
+    """
     new_post = Post(user_id=current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
@@ -57,6 +83,17 @@ def create_post(post: schemas.PostCreateUpdateRequest, db: Session = Depends(get
 @router.put("/{id}", response_model=schemas.PostCreateUpdateResponse)
 def update_post(id: int, updated_post: schemas.PostCreateUpdateRequest, db: Session = Depends(get_db),
                 current_user: int = Depends(get_current_user)):
+    """Update a post
+    Args:
+        id (int): Post ID
+        updated_post (schemas.PostCreateUpdateRequest): Updated post data
+        db (Session, optional): Database session. Defaults to Depends(get_db).
+        current_user (int, optional): Current user. Defaults to Depends(get_current_user).
+    Raises:
+        HTTPException: [description]
+    Returns:
+        Post: Post object
+    """
     post = db.query(Post).filter(Post.id == id)
 
     if post.first() == None:
@@ -70,6 +107,16 @@ def update_post(id: int, updated_post: schemas.PostCreateUpdateRequest, db: Sess
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int ,db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
+    """Delete a post
+    Args:
+        id (int): Post ID
+        db (Session, optional): Database session. Defaults to Depends(get_db).
+        current_user (int, optional): Current user. Defaults to Depends(get_current_user).
+    Raises:
+        HTTPException: [description]
+    Returns:
+        Response: Response object
+    """
     post = db.query(Post).filter(Post.id == id)
 
     if post.first() == None:
